2d0b7f2d6792a25ed41afcf197c9b5f6070af8b0
xanzure <xandersl8r@gmail.com>
2026-07-13 13:20:31 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int front = 0;
+        int back = s.length()-1;
+        while(front < back)
+        {
+            while(!isalnum(s[front])){front++;}
+            while(!isalnum(s[back])){back--;}
+            if(tolower(s[front])!=tolower(s[back]) && front <= back){cout << front << back; return false;}
+            front++;
+            back--;
+        }
+        return true;
+    }
+};
