9171cd1c235235d0077051fac81d8c15c27f2f65
xanzure <xandersl8r@gmail.com>
2026-07-20 19:33:08 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        const unordered_map<char, char> key = {
+            {'}', '{'},
+            {')', '('},
+            {']', '['}
+        };
+        if(s.size()%2 != 0){return false;}
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.size() > 0 && key.at(s[i]) == st.top()){
+                st.pop();
+            } else {return false;}
+        }
+        return (st.empty());
+    }
+};
